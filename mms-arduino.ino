53e9e09d5483689c3c810dbc430bbbd9c8cf9c46
Mack Ward <mackorone@gmail.com>
2020-03-30 17:52:59 -0700

    Remove extraneous setWall call

--- mms-arduino.ino
+++ mms-arduino.ino
@@ -6,7 +6,6 @@ void loop() {
     log("Running...");
     setColor(0, 0, 'G');
     setText(0, 0, "abc");
-    setWall(0, 0, 'e');
     while (true) {
         if (!wallLeft()) {
             turnLeft();
